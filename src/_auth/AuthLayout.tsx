06d92943dc6dc59c41cefb56e50678da0a92e028
Badrio1707 <badrio.yt.gaming@gmail.com>
2025-07-10 15:31:04 +0700

    Auth Screen

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Outlet, Navigate } from "react-router-dom"
 
 const AuthLayout = () => {
+  const isAuthenticated = false; // Replace with actual authentication logic
+
   return (
-    <div>AuthLayout</div>
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" replace />
+      ) : (
+        <section>
+          <Outlet />
+        </section>
+      )}
+    </>
   )
 }
 
--- src/_auth/form/SigninForm.tsx
+++ src/_auth/form/SigninForm.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 const SigninForm = () => {
   return (
-    <div>SigninForm</div>
+    <Card className="sm:w-80 lg:w-96">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>
+          Masukkan email Anda di bawah ini untuk masuk ke akun Anda
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="example@gmail.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+        <CardDescription>
+          Belum punya akun?{" "}
+          <a href="/sign-up" className="text-blue-500 hover:underline">
+            Daftar
+          </a>
+        </CardDescription>
+      </CardFooter>
+    </Card>
   )
 }
 
 export default SigninForm
+
