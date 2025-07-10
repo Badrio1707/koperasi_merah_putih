84860d69695a56f39ef4c8d5ad697e26f2993026
Badrio1707 <badrio.yt.gaming@gmail.com>
2025-07-10 14:57:43 +0700

    Setup, Routing, File & Folder Structure

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from 'react-router-dom'
+import "./global.css"
+import SigninForm from './_auth/form/SigninForm'
+import { Home } from './_root/pages'
+import SignupForm from './_auth/form/SignupForm'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
+
+function App() {
+  return (
+    <main className='flex h-screen w-screen items-center justify-center'>
+        <Routes>
+            {/* public routes */}
+            <Route element={<AuthLayout />}>
+              <Route path="/sign-in" element={<SigninForm />} />
+              <Route path="/sign-up" element={<SignupForm />} />
+            </Route>
+
+            {/* private routes */}
+            <Route element={<RootLayout />}>
+              <Route index element={<Home />} />
+            </Route>
+        </Routes>
+    </main>
+  )
+}
+
+export default App
